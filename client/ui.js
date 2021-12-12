3fc91547e3bf3d239e716c5524c9d2f0e0f87e9c
hhomsi <34064123+hhomsi@users.noreply.github.com>
2021-12-12 21:43:18 +0300

    Update ui.js

--- client/ui.js
+++ client/ui.js
@@ -1,3 +1,3 @@
 const ppGetValue = document.getElementById('mm-connect')
-ppGetValue.onclick = async () => {
-  getPoolsAndMembersDetails();}
+ppGetValue.onclick = () => {
+  alert("Hi! this is test2");}
